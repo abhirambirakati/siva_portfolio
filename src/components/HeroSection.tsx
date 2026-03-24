6cd59b5bf916ea67cb6cbd2ddaa498d089395d3c
abhskfnd@gmail.com <abhirambirakati.com>
2026-03-25 00:33:22 +0530

    portfolio

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import { Github, Linkedin, Mail, Download, ChevronDown } from "lucide-react";
-import profileImg from "@/assets/profile.jpg";
+import profileImg from "@/assets/profile.jpeg";
 
 const HeroSection = () => {
   return (
